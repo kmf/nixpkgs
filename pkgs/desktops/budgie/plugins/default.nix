16c8aa285030cf652363d9b3e5a940e4a5c036e6
Karl Fischer <kmf@kmf.co>
2024-06-25 09:25:55 +0200

    Updated included plugins

--- pkgs/desktops/budgie/plugins/default.nix
+++ pkgs/desktops/budgie/plugins/default.nix
@@ -1,5 +1,6 @@
 { callPackage }: {
   budgie-analogue-clock-applet = callPackage ./budgie-analogue-clock-applet { };
   budgie-media-player-applet = callPackage ./budgie-media-player-applet { };
+  budgie-systemmonitor-applet = callPackage ./budgie-systemmonitor-applet { };
   budgie-user-indicator-redux = callPackage ./budgie-user-indicator-redux { };
 }
